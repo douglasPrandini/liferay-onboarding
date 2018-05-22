d6b9785b1d7b6ef99deb5cb9298c6e75730fbc65
Douglas <prandini@gmail.com>
2018-05-22 17:31:42 -0300

    add reference

--- modules/amf.log-monitor-portlet/src/main/java/com/onboarding/exercises/events/LogoutPostAction.java
+++ modules/amf.log-monitor-portlet/src/main/java/com/onboarding/exercises/events/LogoutPostAction.java
@@ -3,6 +3,7 @@ package com.onboarding.exercises.events;
 
 import com.liferay.docs.amf.registration.sb.dto.AmfRegistrationAuditLogDTO;
 import com.liferay.docs.amf.registration.sb.enums.ActionType;
+import com.liferay.docs.amf.registration.sb.service.AmfRegistrationAuditLogLocalService;
 import com.liferay.docs.amf.registration.sb.service.AmfRegistrationAuditLogLocalServiceUtil;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.LifecycleAction;
@@ -11,6 +12,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.util.PortalUtil;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,6 +27,10 @@ import javax.servlet.http.HttpServletRequest;
 
 public class LogoutPostAction implements LifecycleAction {
 
+    @Reference
+    AmfRegistrationAuditLogLocalService _amfRegistrationAuditLogLocalService;
+
+
     @Override
     public void processLifecycleEvent(LifecycleEvent lifecycleEvent) throws ActionException {
         HttpServletRequest request = lifecycleEvent.getRequest();
@@ -37,7 +43,7 @@ public class LogoutPostAction implements LifecycleAction {
             amfRegistrationAuditLogDTO.setScreen_name(user.getScreenName());
             amfRegistrationAuditLogDTO.setUser_id(user.getUserId());
 
-            AmfRegistrationAuditLogLocalServiceUtil.addNewRegister(amfRegistrationAuditLogDTO);
+            _amfRegistrationAuditLogLocalService.addNewRegister(amfRegistrationAuditLogDTO);
         } catch (PortalException e) {
             e.printStackTrace();
         }
